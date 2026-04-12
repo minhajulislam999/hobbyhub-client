7d95e94e9cd75d5bb44ed5581e58fce88a0d6cc1
minhajul islam <mm01882390860@gmail.com>
2026-04-13 03:21:42 +0600

    added 404 not found page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
+import { Link } from "react-router-dom";
 
 const NotFound = () => {
   return (
-    <div>
-      <h1>404</h1>
-      <p>Page Not Found</p>
+    <div className="min-h-screen flex flex-col items-center justify-center">
+      <h1 className="text-9xl font-bold text-primary">404</h1>
+      <h2 className="text-3xl font-semibold mt-4">Page Not Found</h2>
+      <p className="text-gray-500 mt-2">The page you are looking for does not exist.</p>
+      <Link to="/" className="btn btn-primary mt-6">Go Back Home</Link>
     </div>
-  )
-}
+  );
+};
 
-export default NotFound
+export default NotFound;
